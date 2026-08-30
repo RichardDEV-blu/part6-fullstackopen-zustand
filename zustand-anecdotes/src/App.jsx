e41c36a05613d62b254414daee14642916068909
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-29 20:16:29 -0500

    part 6: 6.2 - 6.3

---
+++ zustand-anecdotes/src/App.jsx
@@ -0,0 +1,37 @@
+
+import { useAnecdotes, useAnecdoteActions } from './store'
+
+const App = () => {
+  const anecdotes = useAnecdotes()
+  const { vote, create } = useAnecdoteActions()
+
+  const createAnecdote = event => {
+    event.preventDefault()
+    const content = new FormData(event.target).get('content')
+    create(content)
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={createAnecdote}>
+        <div>
+          <input name="content" />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
