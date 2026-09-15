ba94ba5b19669a6afe8f0aff3abb7b0c2cd2558f
RichardDEV-blu <arteagaricharda@gmail.com>
2026-09-15 15:09:35 -0500

    part 6: 6.20

---
+++ query-anecdotes/src/NotificationContext.jsx
@@ -0,0 +1,25 @@
+import { useState, createContext } from 'react'
+
+const NotificationContext = createContext()
+
+export const NotificationContextProvider = ({ children }) => {
+    const [notification, setNotification] = useState(null)
+
+    const notify = (message) => {
+        setNotification(message)
+
+        setTimeout(() => {
+            setNotification(null)
+        }, 5000)
+    }
+
+    return (
+        <NotificationContext.Provider
+            value={{ notification, notify }}>
+            {children}
+        </NotificationContext.Provider>
+
+    )
+}
+
+export default NotificationContext
--- query-anecdotes/src/hooks/useAnecdotes.js
+++ query-anecdotes/src/hooks/useAnecdotes.js
@@ -1,5 +1,7 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { getAnecdotes, updateAnecdote, createAnecdote } from '../requests'
+import { useContext } from 'react'
+import NotificationContext from '../NotificationContext'
 
 export const useAnecdotes = () =>
     useQuery({
@@ -12,25 +14,30 @@ export const useAnecdotes = () =>
 
 export const useVoteMutation = () => {
     const queryClient = useQueryClient()
-
+    const { notify } = useContext(NotificationContext)
     return useMutation({
         mutationFn: updateAnecdote,
         onSuccess: () => {
             queryClient.invalidateQueries({
                 queryKey: ['anecdotes']
             })
+            notify('you voted for the anecdote')
         }
     })
 }
 
 export const useCreateMutation = () => {
     const queryClient = useQueryClient()
+
+    const { notify } = useContext(NotificationContext)
+
     return useMutation({
         mutationFn: createAnecdote,
         onSuccess: () => {
             queryClient.invalidateQueries({
                 queryKey: ['anecdotes']
             })
+            notify('anecdote created')
         }
     })
 }
